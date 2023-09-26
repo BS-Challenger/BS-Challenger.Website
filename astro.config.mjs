cb2a79c418d542fa9bafe4a5f5c928ec238a7e94
Futuremappermydud <54294576+Futuremappermydud@users.noreply.github.com>
2023-09-25 20:30:35 -0400

    Add levelcard for ranking page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 import { defineConfig } from 'astro/config';
-import node from "@astrojs/node";
-
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [react()],
   output: "server"
